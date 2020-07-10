b3f0da9938679c4e997d56b0e1bd98e5875fdaa9
ford@AcerSF114 <-@->
2020-07-10 19:32:08 +0100

    clean up

--- UbuntuInit/InitUbuntu20.04.sh
+++ UbuntuInit/InitUbuntu20.04.sh
@@ -107,17 +107,6 @@ sudo apt-get -y clean && \
 
 # echo '' && \
 # 	echo '====================================' && \
-# 	echo 'INSTALL SUBLIME TEXT' && \
-# 	echo '===================================='
-# wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - && \
-# 	sudo apt-get install -y apt-transport-https && \
-# 	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list && \
-# 	sudo apt-get update && \
-# 	sudo apt-get install -y sublime-text
-
-
-# echo '' && \
-# 	echo '====================================' && \
 # 	echo 'APT-GET INSTALLS' && \
 # 	echo '===================================='
 # sudo apt-get install -y gparted && \
