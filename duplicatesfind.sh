5a0335181a51482ad042ea001cdf970631df8888
df@ <@>
2021-04-17 13:25:25 +0100

    __wip__ duplaicate finder

--- duplicatesfind.sh
+++ duplicatesfind.sh
@@ -1,5 +1,21 @@
 # time find photos/ ! -path '**.git/*' ! -empty -type f -exec md5sum    {} \; | sort | uniq -w 32  --all-repeated=separate
 # time find photos/ ! -path '**.git/*' ! -empty -type f -exec sha512sum {} \; | sort | uniq -w 128 --all-repeated=separate
 # time find ~/storage/photos* -type f -exec sha512sum {} \; | sort | uniq -w 128 --all-repeated=separate > DUPLICATES.sh
-time find ~/storage/Music -type f -exec sha512sum {} \; | sort | uniq -w 128 --all-repeated=separate > DUPLICATES.sh
+# time find ~/storage/Music -type f -exec sha512sum {} \; | sort | uniq -w 128 --all-repeated=separate > DUPLICATES.sh
 ###################################
+
+time find \
+    ~/storage -type f \
+    ! -path '**.git/*' \
+    ! -path '**.venv/*' \
+    ! -path '**dev/*' \
+    ! -path '**.Trash-1000/*' \
+    ! -path '**Dropbox/*' \
+    ! -path '**DUPLICATES/*' \
+    -exec sha512sum {} \; \
+    | tee DUPLICATES2.sh
+
+
+sort DUPLICATES2.sh | tee DUPLICATES2.sort.sh
+
+uniq -w 128 --all-repeated=separate DUPLICATES2.sort.sh | tee DUPLICATES2.uniq.sh
