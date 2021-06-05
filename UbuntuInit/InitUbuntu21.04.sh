4eb776e760e60549833203876b784594480a6096
df@ <@>
2021-06-05 19:42:25 +0100

    init 21

---
+++ UbuntuInit/InitUbuntu21.04.sh
@@ -0,0 +1,7 @@
+# Backup original sys files
+sudo cp /etc/fstab /etc/fstab.ORIGINAL
+sudo cp /etc/default/grub /etc/default/grub.ORIGINAL
+sudo cp /etc/adduser.conf /etc/adduser.conf.ORIGINAL
+sudo cp /etc/sysctl.conf /etc/sysctl.conf.ORIGINAL
+sudo cp /etc/apt/apt.conf.d/20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades.ORIGINAL
+# sudo cp /etc/default/motd-news /etc/default/motd-news.ORIGINAL
