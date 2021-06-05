84804756f91cbb2c1f4307d33e6ff5d0c08c0087
df@ <@>
2021-06-05 20:26:54 +0100

    ubu 21 init

--- UbuntuInit/InitUbuntu21.04.sh
+++ UbuntuInit/InitUbuntu21.04.sh
@@ -1,6 +1,7 @@
-# ##########
+# ######################################################################
+# ######################################################################
 # Backup original sys files
-# ##########
+
 sudo cp /etc/fstab /etc/fstab.ORIGINAL
 sudo cp /etc/default/grub /etc/default/grub.ORIGINAL
 sudo cp /etc/adduser.conf /etc/adduser.conf.ORIGINAL
@@ -8,8 +9,26 @@ sudo cp /etc/sysctl.conf /etc/sysctl.conf.ORIGINAL
 sudo cp /etc/apt/apt.conf.d/20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades.ORIGINAL
 
 # check these:
+
 # doesn't exist:
-# sudo cp /etc/default/motd-news /etc/default/motd-news.ORIGINAL
+# /etc/default/motd-news
+
 # found:
 # /etc/systemd/system/timers.target.wants/motd-news.timer
 # /etc/update-motd.d/50-motd-news
+
+
+
+# ######################################################################
+# ######################################################################
+
+# Set swappiness manually in "/etc/sysctl.conf"
+# vm.swappiness=30
+
+
+
+# ######################################################################
+# ######################################################################
+# Sys settings
+
+sudo sed -i 's:GRUB_CMDLINE_LINUX_DEFAULT="quiet splash":GRUB_CMDLINE_LINUX_DEFAULT="":g' /etc/default/grub && sudo update-grub
