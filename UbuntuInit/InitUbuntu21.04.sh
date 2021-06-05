b67f38c0a616440130557b5722aee316eadeb5e4
df@ <@>
2021-06-05 20:16:26 +0100

    init 21

--- UbuntuInit/InitUbuntu21.04.sh
+++ UbuntuInit/InitUbuntu21.04.sh
@@ -4,4 +4,10 @@ sudo cp /etc/default/grub /etc/default/grub.ORIGINAL
 sudo cp /etc/adduser.conf /etc/adduser.conf.ORIGINAL
 sudo cp /etc/sysctl.conf /etc/sysctl.conf.ORIGINAL
 sudo cp /etc/apt/apt.conf.d/20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades.ORIGINAL
+
+# check these:
+# doesn't exist:
 # sudo cp /etc/default/motd-news /etc/default/motd-news.ORIGINAL
+# found:
+# /etc/systemd/system/timers.target.wants/motd-news.timer
+# /etc/update-motd.d/50-motd-news
