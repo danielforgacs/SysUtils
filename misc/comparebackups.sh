d08f8a002e6402c5abf54e7a9cfd917535ed3fb1
df@ <@>
2021-06-05 01:22:13 +0100

    added misc

---
+++ misc/comparebackups.sh
@@ -0,0 +1,223 @@
+backup_A='/media/ford/BACKUP'
+backup_B='/media/ford/FORD_STORAGE_PORTABLE'
+
+			# date; time diff -q -r "$backup_A/Cinema - youtube"						"$backup_B/Cinema - youtube"
+			# echo "======= DONE: 14"
+
+			# date; time diff -q -r "$backup_A/___20_BACKUP___12-Dec_2017"			"$backup_B/___20_BACKUP___12-Dec_2017"
+			# echo "======= DONE: 1"
+
+			# date; time diff -q -r "$backup_A/___21_BACKUP___13_May_2018"			"$backup_B/___21_BACKUP___13_May_2018"
+			# echo "======= DONE: 2"
+
+			# date; time diff -q -r "$backup_A/___22_BACKUP___15_July_2018"			"$backup_B/___22_BACKUP___15_July_2018"
+			# echo "======= DONE: 3"
+
+			# date; time diff -q -r "$backup_A/___23_BACKUP___06_Oct_2018"			"$backup_B/___23_BACKUP___06_Oct_2018"
+			# echo "======= DONE: 4"
+
+			# date; time diff -q -r "$backup_A/__backup_025___2020-02-02_20-56-02"	"$backup_B/__backup_025___2020-02-02_20-56-02"
+			# echo "======= DONE: 7"
+
+			# date; time diff -q -r "$backup_A/__backup_026___2020-03-01_14-40-57"	"$backup_B/__backup_026___2020-03-01_14-40-57"
+			# echo "======= DONE: 8"
+
+			# date; time diff -q -r "$backup_A/__backup_027___2020-03-30_13-28-44"	"$backup_B/__backup_027___2020-03-30_13-28-44"
+			# echo "======= DONE: 9"
+
+			# date; time diff -q -r "$backup_A/__backup_028___2020-04-29_16-24-15"	"$backup_B/__backup_028___2020-04-29_16-24-15"
+			# echo "======= DONE: 10"
+
+			# date; time diff -q -r "$backup_A/Egy rem rendes csalad"					"$backup_B/Egy rem rendes csalad"
+			# echo "======= DONE: 16"
+
+			# date; time diff -q -r "$backup_A/ISO"									"$backup_B/ISO"
+			# echo "======= DONE: 18"
+
+			# date; time diff -q -r "$backup_A/Magyar Filmalap - Vimeo"				"$backup_B/Magyar Filmalap - Vimeo"
+			# echo "======= DONE: 19"
+
+			# date; time diff -q -r "$backup_A/programming videos from Michael"		"$backup_B/programming videos from Michael"
+			# echo "======= DONE: 20"
+
+			# date; time diff -q -r "$backup_A/Tutorial Videos - youtube"				"$backup_B/Tutorial Videos - youtube"
+			# echo "======= DONE: 22"
+
+			# date; time diff -q -r "$backup_A/Tech Videos - youtube"					"$backup_B/Tech Videos - youtube"
+			# echo "======= DONE: 23"
+
+			# date; time diff -q -r "$backup_A/UNCLE_BOB"								"$backup_B/UNCLE_BOB"
+			# echo "======= DONE: 24"
+
+			# date; time diff -q -r "$backup_A/Burrows Tutorial videos"				"$backup_B/Burrows Tutorial videos"
+			# echo "======= DONE: 27"
+
+			# # 261G	/media/ford/BACKUP/___24_BACKUP___21_April_2019/
+			# du -h -d 1 "$backup_A/___24_BACKUP___21_April_2019"
+			# date; time diff -q -r "$backup_A/___24_BACKUP___21_April_2019"			"$backup_B/___24_BACKUP___21_April_2019"
+			# echo "======= DONE: 5"
+
+			# # 22G	/media/ford/BACKUP/__NEXUS-6P__/
+			# date; time diff -q -r "$backup_A/__NEXUS-6P__"							"$backup_B/__NEXUS-6P__"
+			# echo "======= DONE: 12"
+
+			# # 57G	/media/ford/BACKUP/Django - youtube/
+			# date; time diff -q -r "$backup_A/Django - youtube"						"$backup_B/Django - youtube"
+			# echo "======= DONE: 17"
+
+			# # 56G	/media/ford/BACKUP/__DEJA_DUPE_BACKUP__001/
+			# du -h -d 0 "$backup_A/__DEJA_DUPE_BACKUP__001"
+			# date; time diff -q -r "$backup_A/__DEJA_DUPE_BACKUP__001"				"$backup_B/__DEJA_DUPE_BACKUP__001"
+			# echo "======= DONE: 11"
+
+			# # 54G	/media/ford/BACKUP/Python - youtube/
+			# date; time diff -q -r "$backup_A/Python - youtube"						"$backup_B/Python - youtube"
+			# echo "======= DONE: 21"
+
+			# # 76G	/media/ford/BACKUP/__001-SERI_BACKUP--23-11-2019/
+			# date; time diff -q -r "$backup_A/__001-SERI_BACKUP--23-11-2019"			"$backup_B/__001-SERI_BACKUP--23-11-2019"
+			# echo "======= DONE: 25"
+
+			# +++++++
+			# +++++++
+			# +++++++
+			# 198G	/media/ford/BACKUP/___BACKUP_ACER_SWIFT_1/
+			# date; time diff -q "$backup_A/___BACKUP_ACER_SWIFT_1"				"$backup_B/___BACKUP_ACER_SWIFT_1"
+			# date; time diff -q -r "$backup_A/___BACKUP_ACER_SWIFT_1"				"$backup_B/___BACKUP_ACER_SWIFT_1"
+
+			# date; time diff -q -r "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-04-22_16-47-11" "$backup_B/___BACKUP_ACER_SWIFT_1/backup_2019-04-22_16-47-11"
+			# echo "======= DONE: 6_1"
+
+			# date; time diff -q -r "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-04-26_19-41-20" "$backup_B/___BACKUP_ACER_SWIFT_1/backup_2019-04-26_19-41-20"
+			# echo "======= DONE: 6_2"
+
+			# date; time diff -q -r "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-04-27_10-52-49" "$backup_B/___BACKUP_ACER_SWIFT_1/backup_2019-04-27_10-52-49"
+			# echo "======= DONE: 6_3"
+
+			# date; time diff -q -r "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-05-05_13-01-08" "$backup_B/___BACKUP_ACER_SWIFT_1/backup_2019-05-05_13-01-08"
+			# echo "======= DONE: 6_4"
+
+			# date; time diff -q -r "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-05-05_17-16-01" "$backup_B/___BACKUP_ACER_SWIFT_1/backup_2019-05-05_17-16-01"
+			# echo "======= DONE: 6_5"
+
+			# date; time diff -q -r "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-06-02_17-16-43" "$backup_B/___BACKUP_ACER_SWIFT_1/backup_2019-06-02_17-16-43"
+			# echo "======= DONE: 6_6"
+
+			# date; time diff -q -r "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-06-09_20-09-12" "$backup_B/___BACKUP_ACER_SWIFT_1/backup_2019-06-09_20-09-12"
+			# echo "======= DONE: 6_7"
+
+			# du -h -d 0 "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-08-15_21-25-36"			
+			# date; time diff -q -r "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-08-15_21-25-36" "$backup_B/___BACKUP_ACER_SWIFT_1/backup_2019-08-15_21-25-36"
+			# echo "======= DONE: 6_8"
+
+			# du -h -d 0 "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-08-27_23-42-53"
+			# date; time diff -q -r "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-08-27_23-42-53" "$backup_B/___BACKUP_ACER_SWIFT_1/backup_2019-08-27_23-42-53"
+			# echo "======= DONE: 6_9"
+
+			# du -h -d 0 "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-09-08_20-28-43"
+			# date; time diff -q -r "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-09-08_20-28-43" "$backup_B/___BACKUP_ACER_SWIFT_1/backup_2019-09-08_20-28-43"
+			# echo "======= DONE: 6_10"
+
+			# du -h -d 0 "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-09-12_01-08-18"
+			# date; time diff -q -r "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-09-12_01-08-18" "$backup_B/___BACKUP_ACER_SWIFT_1/backup_2019-09-12_01-08-18"
+			# echo "======= DONE: 6_11"
+
+			# du -h -d 0 "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-09-24_21-26-34"
+			# date; time diff -q -r "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-09-24_21-26-34" "$backup_B/___BACKUP_ACER_SWIFT_1/backup_2019-09-24_21-26-34"
+			# echo "======= DONE: 6_12"
+
+			# du -h -d 0 "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-10-05_14-23-40"
+			# date; time diff -q -r "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-10-05_14-23-40" "$backup_B/___BACKUP_ACER_SWIFT_1/backup_2019-10-05_14-23-40"
+			# echo "======= DONE: 6_13"
+
+			# du -h -d 0 "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-10-06_21-12-44"
+			# date; time diff -q -r "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-10-06_21-12-44" "$backup_B/___BACKUP_ACER_SWIFT_1/backup_2019-10-06_21-12-44"
+			# echo "======= DONE: 6_14"
+
+			# du -h -d 0 "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-10-28_20-35-43"
+			# date; time diff -q -r "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-10-28_20-35-43" "$backup_B/___BACKUP_ACER_SWIFT_1/backup_2019-10-28_20-35-43"
+			# echo "======= DONE: 6_15"
+
+			# du -h -d 0 "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-11-19_23-08-41"
+			# date; time diff -q -r "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-11-19_23-08-41" "$backup_B/___BACKUP_ACER_SWIFT_1/backup_2019-11-19_23-08-41"
+			# echo "======= DONE: 6_16"
+
+			# du -h -d 0 "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-12-10_10-55-44"
+			# date; time diff -q -r "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-12-10_10-55-44" "$backup_B/___BACKUP_ACER_SWIFT_1/backup_2019-12-10_10-55-44"
+			# echo "======= DONE: 6_17"
+
+			# du -h -d 0 "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-12-17_18-21-24"
+			# date; time diff -q -r "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-12-17_18-21-24" "$backup_B/___BACKUP_ACER_SWIFT_1/backup_2019-12-17_18-21-24"
+			# echo "======= DONE: 6_18"
+
+			# du -h -d 0 "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-12-23_14-04-35"
+			# date; time diff -q -r "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2019-12-23_14-04-35" "$backup_B/___BACKUP_ACER_SWIFT_1/backup_2019-12-23_14-04-35"
+			# echo "======= DONE: 6_19"
+
+			# du -h -d 0 "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2020-01-04_17-12-35"
+			# date; time diff -q -r "$backup_A/___BACKUP_ACER_SWIFT_1/backup_2020-01-04_17-12-35" "$backup_B/___BACKUP_ACER_SWIFT_1/backup_2020-01-04_17-12-35"
+			# echo "======= DONE: 6_10"
+
+			# echo "======= DONE: 6"
+
+# # 605G	/media/ford/BACKUP/Cinema/
+# date; time diff -q -r "$backup_A/Cinema"								"$backup_B/Cinema"
+# echo "======= DONE: 15"
+
+# # 362G	/media/ford/BACKUP/_BACKUP-No_Copy/
+# date; time diff -q -r "$backup_A/_BACKUP-No_Copy"						"$backup_B/_BACKUP-No_Copy"
+# echo "======= DONE: 13"
+
+# # 210G	/media/ford/BACKUP/Houdini Tutor/
+# date; time diff -q -r "$backup_A/Houdini Tutor"							"$backup_B/Houdini Tutor"
+# echo "======= DONE: 28"
+
+# date; time diff -q -r "$backup_A/.x"									"$backup_B/.x"
+# echo "======= DONE: 26"
+
+
+# ...............................................
+
+# backup_A='/media/ford/BACKUP'
+# backup_B='/media/ford/FORD_STORAGE_PORTABLE'
+
+# time diff -q \
+# 	-r \
+# 	$backup_A/ $backup_B/ \
+# 	-x __001-SERI_BACKUP--23-11-2019 \
+# 	-x .x \
+# 	-x "Burrows Tutorial videos" \
+# 	-x "Django - youtube" \
+# 	-x "Houdini Tutor" \
+# 	-x ISO \
+# 	-x .Trash*
+
+
+# -------------------------------------------------
+
+# file:///media/ford/BACKUP/__NEXUS 6P__
+
+# pendrive='/media/ford/FORD_USB_BACKUP/_____COPIED'
+# # bkp_b='/media/ford/FORD_STORAGE_PORTABLE'
+# bkp_b='/media/ford/BACKUP'
+
+
+# echo "===== 111 ========"
+# date
+# time diff -q -r $bkp_b/__backup_025___2020-02-02_20-56-02/ $pendrive/__BACKUP__/backup_025___2020-02-02_20-56-02/
+
+# echo "===== 222 ========"
+# date
+# time diff -q -r $bkp_b/__backup_026___2020-03-01_14-40-57/ $pendrive/__BACKUP__/backup_026___2020-03-01_14-40-57/
+
+# echo "===== 333 ========"
+# date
+# time diff -q -r $bkp_b/__backup_027___2020-03-30_13-28-44/ $pendrive/__BACKUP__/backup_027___2020-03-30_13-28-44/
+
+# echo "===== 444 ========"
+# date
+# time diff -q -r $bkp_b/__backup_028___2020-04-29_16-24-15/ $pendrive/__BACKUP__/backup_028___2020-04-29_16-24-15/
+
+# echo "===== 555 ========"
+# date
+# time diff -q -r $bkp_b/__DEJA_DUPE_BACKUP__001/ $pendrive/__DEJA_DUPE_BACKUP__/
