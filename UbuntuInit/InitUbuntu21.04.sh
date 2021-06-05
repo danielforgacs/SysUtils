ca976c93ee34d1d145d0ef719b53785436ca4e85
df@ <@>
2021-06-05 20:51:56 +0100

    init

--- UbuntuInit/InitUbuntu21.04.sh
+++ UbuntuInit/InitUbuntu21.04.sh
@@ -8,7 +8,7 @@ sudo cp /etc/adduser.conf /etc/adduser.conf.ORIGINAL
 sudo cp /etc/sysctl.conf /etc/sysctl.conf.ORIGINAL
 sudo cp /etc/apt/apt.conf.d/20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades.ORIGINAL
 
-# check these:
+# check these, these are used to fetch some news on updates...
 
 # doesn't exist:
 # /etc/default/motd-news
@@ -25,10 +25,23 @@ sudo cp /etc/apt/apt.conf.d/20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades.
 # Set swappiness manually in "/etc/sysctl.conf"
 # vm.swappiness=30
 
+# check:
+# $ cat /proc/sys/vm/swappiness
+
 
 
 # ######################################################################
 # ######################################################################
 # Sys settings
 
-sudo sed -i 's:GRUB_CMDLINE_LINUX_DEFAULT="quiet splash":GRUB_CMDLINE_LINUX_DEFAULT="":g' /etc/default/grub && sudo update-grub
+sudo sed -i 's:GRUB_CMDLINE_LINUX_DEFAULT="quiet splash":GRUB_CMDLINE_LINUX_DEFAULT="":g' /etc/default/grub
+sudo update-grub
+
+# some unnecessary new update...
+# sudo sed -i 's:Update-Package-Lists "1":Update-Package-Lists "0":g' /etc/apt/apt.conf.d/20auto-upgrades
+# sudo sed -i 's:Unattended-Upgrade "1":Unattended-Upgrade "0":g' /etc/apt/apt.conf.d/20auto-upgrades
+
+# New user dir permission settings... ! ubuntu defaults changed !
+# sudo sed -i 's:DIR_MODE=0755:DIR_MODE=700:g' /etc/adduser.conf
+# sudo sed -i "s:ENABLED=1:# ENABLED=0:g" /etc/default/motd-news
+# sudo sed -i "s:URLS=\"https:# URLS=\"https:g" /etc/default/motd-news
