3cb271753acd800a2a4625d25d15b7bb07460a25
df@ <@>
2021-06-07 13:55:02 +0100

    wip

--- UbuntuInit/sysdiag.py
+++ UbuntuInit/sysdiag.py
@@ -1,15 +1,33 @@
 import subprocess
+import io
+import sys
 
 
 EXPECTEDSWAPPINESS = '30'
+
+
+class StdOutCapture:
+    def __enter__(self, *args, **kwargs):
+        self.stdout = io.StringIO()
+        sys.stdout = self.stdout
+
+    def __exit__(self, *args, **kwargs):
+        sys.stdout == sys.__stdout__
+
+    @property
+    def data(self):
+        return self.stdout.getvalue()
+
 
 
 def check_swappiness():
     cmd = ['cat', '/proc/sys/vm/swappiness']
     swappinesscheck = subprocess.run(cmd, capture_output=True)
     swappinessnum = swappinesscheck.stdout.decode().strip()
+    msg = None
 
     if swappinessnum != EXPECTEDSWAPPINESS:
+        # msg =
         # error('swappiness is: '+swappinessnum+'; expected: '+EXPECTEDSWAPPINESS)
         print('swappiness is: '+swappinessnum+'; expected: '+EXPECTEDSWAPPINESS)
 
@@ -17,4 +35,8 @@ def check_swappiness():
 
 
 if __name__ == '__main__':
+    # sys.stdout = io.StringIO()
+    # mysdtout = sys.stdout
     check_swappiness()
+    # sys.stdout = sys.__stdout__
+    # print(mysdtout.getvalue())
