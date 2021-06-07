0be7a2d267d34a8f44b655c518b4a32edcbdad6b
df@ <@>
2021-06-07 14:16:17 +0100

    sysdiag ok

--- UbuntuInit/sysdiag.py
+++ UbuntuInit/sysdiag.py
@@ -6,37 +6,29 @@ import sys
 EXPECTEDSWAPPINESS = 30
 
 
-class StdOutCapture:
-    def __enter__(self, *args, **kwargs):
-        self.stdout = io.StringIO()
-        sys.stdout = self.stdout
-        return self
-
-    def __exit__(self, *args, **kwargs):
-        sys.stdout = sys.__stdout__
-
-    @property
-    def data(self):
-        return self.stdout.getvalue().strip()
+def print_result(func):
+    def wrapper():
+        result = func()
+        print(result)
+        return result
+    return wrapper
 
 
-
+@print_result
 def check_swappiness():
     cmd = ['cat', '/proc/sys/vm/swappiness']
     swappinesscheck = subprocess.run(cmd, capture_output=True)
-    swappinessnum = swappinesscheck.stdout.decode().strip()
-    msg = None
+    swappinessnum = int(swappinesscheck.stdout.decode().strip())
+    msg = '[ERROR] swappiness OK.'
 
     if swappinessnum != EXPECTEDSWAPPINESS:
-        # msg =
-        # error('swappiness is: '+swappinessnum+'; expected: '+EXPECTEDSWAPPINESS)
-        print('swappiness is: '+swappinessnum+'; expected: '+str(EXPECTEDSWAPPINESS))
+        msg = '[ERROR] swappiness is: '+str(swappinessnum)+'; expected: '+str(EXPECTEDSWAPPINESS)
+
+    return msg
 
 
 
 
 if __name__ == '__main__':
-    with StdOutCapture() as capt:
-        check_swappiness()
-        data = capt.data
+    data = check_swappiness()
     print(data)
