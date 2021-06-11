ab722f2a076d592a9727830e28d466ce6385073a
df@ <@>
2021-06-11 18:45:48 +0100

    OK

--- rnd/sysdiagreport.py
+++ rnd/sysdiagreport.py
@@ -1,20 +1,20 @@
 from wsgiref.validate import validator
 from wsgiref.simple_server import make_server
 
-# Our callable object which is intentionally not compliant to the
-# standard, so the validator is going to break
-def simple_app(environ, start_response):
-    status = '200 OK'  # HTTP Status
-    headers = [('Content-type', 'text/plain')]  # HTTP Headers
-    start_response(status, headers)
+STATUS_OK = '200 OK'
+HEADERS = [('Content-type', 'text/plain')]
 
-    # This is going to break because we need to return a list, and
-    # the validator is going to inform us
-    return [b"Hello World"]
 
-# This is the application wrapped in a validator
-# validator_app = validator(simple_app)
+def route_request_urls(environ, start_response):
+    response = [b"Hello World"]
+    start_response(STATUS_OK, HEADERS)
 
-with make_server('', 8000, simple_app) as httpd:
-    print("Listening on port 8000....")
-    httpd.serve_forever()
+    return response
+
+
+if __name__ == '__main__':
+    validator_app = validator(route_request_urls)
+
+    with make_server('', 8000, validator_app) as httpd:
+        print("http://localhost:8000")
+        httpd.serve_forever()
