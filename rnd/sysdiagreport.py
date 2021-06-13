15ec5710c787f52dab70447c194eff9eead0d11d
df@ <@>
2021-06-13 12:03:06 +0100

    ..

--- rnd/sysdiagreport.py
+++ rnd/sysdiagreport.py
@@ -1,3 +1,4 @@
+import datetime
 import wsgiref.validate as validate
 import wsgiref.simple_server as simple_server
 
@@ -6,9 +7,31 @@ PORT = 8000
 STATUS_OK = '200 OK'
 HEADERS = [('Content-type', 'text/html')]
 
+ROOT_HMTL = """
+<script>
+    setInterval(function() {
+      fetch('/')
+    }, 3000);
+</script>
+"""
+
+# await new Promise(func => setTimeout(func, 2000));
+# var intervalId = setInterval(function() {
+#   alert("Interval reached every 5s")
+# }, 5000);
+# setInterval(function() {
+#   console.log('lkjhk')
+# }, 3000);
+
+
 
 def route_request_urls(environ, start_response):
-    response = [b"Hello World"]
+    time = str(datetime.datetime.now())
+    print(time)
+    url = environ['PATH_INFO']
+    print(url)
+    html = time+ROOT_HMTL
+    response = [html.encode()]
     start_response(STATUS_OK, HEADERS)
 
     return response
